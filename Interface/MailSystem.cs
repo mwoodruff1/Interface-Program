b5dd769cb97f9da41b4e8dca0ac9c4b688c7535d
Matthew Woodruff <mwoodruff1@live.maryville.edu>
2018-08-22 20:49:59 -0500

    comments added to mail system

--- Interface/MailSystem.cs
+++ Interface/MailSystem.cs
@@ -14,18 +14,18 @@ namespace Interface
 
         internal void get(){
 
-            client.Connect("pop.gmail.com", 995, true);
-            client.Authenticate("systeminterface01@gmail.com", "Max1Oreo2");
+            client.Connect("pop.gmail.com", 995, true);// Connects to the pop3 server for gmail
+            client.Authenticate("systeminterface01@gmail.com", "Max1Oreo2");// Login Credentials
 
 
 
-            var count = client.GetMessageCount();
-            OpenPop.Mime.Message message = client.GetMessage(count);
-            OpenPop.Mime.MessagePart plainText = message.FindFirstPlainTextVersion();
-            builder.Append(plainText.GetBodyAsText());
-            Console.WriteLine(builder.ToString());
-            builder.Clear();
-            client.Disconnect();
+            var count = client.GetMessageCount();// gets the message count
+            OpenPop.Mime.Message message = client.GetMessage(count);// Gets the message that correlates with the count variable- in this case 0
+            OpenPop.Mime.MessagePart plainText = message.FindFirstPlainTextVersion();// Gets the first messsages body
+            builder.Append(plainText.GetBodyAsText()); //Puts the email's text into a StringBuilder where it can be outputed
+            Console.WriteLine(builder.ToString()); //Outputs the email to the console
+            builder.Clear(); //Clears the String Builder
+            client.Disconnect(); // Disconnects from the Pop3 Client
         
         }
 
